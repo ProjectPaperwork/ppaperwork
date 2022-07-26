028d5361229c7a9fadb2d22374b363f3c768de32
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-07-26 21:27:52 +0200

    Generate ppaperwork image using alpine image

--- work.sh
+++ work.sh
@@ -3,23 +3,21 @@
 USER_ID=${USER_ID:-30000}
 GROUP_ID=${GROUP_ID:-30000}
 
+# script is ran as root, so root!
 if [ "$USER_ID" == 0 ]; then
 	# We shall run everything as root
 	mkdir -p /builder
 
 	GOSU=""
+
+# Host user has already the correct uid
 elif [ "$USER_ID" == "$UID" ]; then
 	GOSU=""
-else
-	if ! grep -q "^builder:" /etc/group; then
-		groupadd -o --gid "$GROUP_ID" builder
-	fi
-	if ! id builder >/dev/null 2>&1; then
-		# Create a non-root user that will perform the actual build
-		useradd -o --uid "$USER_ID" --gid "$GROUP_ID" --create-home \
-			--home-dir /builder builder
-	fi
 
+# Host user and container user don't have the same UID, so create a specific user
+else
+	addgroup -g ${GROUP_ID} builder
+	adduser -D -G builder -h /builder builder
 	GOSU="gosu builder"
 fi
 
@@ -27,13 +25,8 @@ if [ "$PWD" = / ]; then
 	cd /builder || exit 1
 fi
 
-# if [[ -z "$@" ]]; then
-#   echo "no cmd passed" 
-# #   exec $GOSU bash
-# else 
-  cmd="python3 -m gherkin_paperwork"
-  echo "run cmd $cmd" 
-  exec $GOSU bash -c "$cmd"; 
-# fi
+cmd="python3 -m gherkin_paperwork"
+echo "run cmd $cmd" 
+exec $GOSU bash -c "$cmd"; 
 
 
